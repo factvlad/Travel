5b8c397dc9dda0b13b0a900de62faed8ed255e43
factvlad <fact1@ukr.net>
2022-02-20 20:10:03 +0200

    update

--- app/js/main.js
+++ app/js/main.js
@@ -19,14 +19,16 @@ $(function () {
       "<button  type='button' class='slick-next'><svg  class='next' width='37' height='37' viewBox='0 0 37 37' fill='none' xmlns='http://www.w3.org/2000/svg'><rect class='rect-next'width='36' height='36' rx='6' transform='matrix(-1 0 0 1 36.25 0.566406)' fill='white'/><path class='path-next' d='M16.25 24.5664L22.25 18.5664L16.25 12.5664' stroke='#9EAAB7' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/></svg></button>",
   });
 
-
   $(".popular__slider-list").slick({
     dots: false,
     autoplay: true,
     slidesToShow: 3,
     slidesToScroll: 1,
+    prevArrow:
+      "<button type='button' class='slick-prev'><svg  class='prev' width='53' height='53' viewBox='0 0 53 53' fill='none' xmlns='http://www.w3.org/2000/svg'><g filter='url(#filter0_d_376_1053)'><rect class='rect-prev' x='8.5' y='8.56641' width='36' height='36' rx='6' fill='white'/></g><path class='path-prev' d='M28.5 32.5664L22.5 26.5664L28.5 20.5664' stroke='#9EAAB7' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/><defs><filter id='filter0_d_376_1053' x='0.5' y='0.566406' width='52' height='52' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'><feFlood flood-opacity='0' result='BackgroundImageFix'/><feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' result='hardAlpha'/><feOffset/><feGaussianBlur stdDeviation='4'/><feComposite in2='hardAlpha' operator='out'/><feColorMatrix type='matrix' values='0 0 0 0 0.137255 0 0 0 0 0.164706 0 0 0 0 0.203922 0 0 0 0.2 0'/><feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_376_1053'/><feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_376_1053' result='shape'/></filter></defs></svg></button>",
+    nextArrow:
+      "<button type='button' class='slick-next'> <svg class='next' width='53' height='53' viewBox='0 0 53 53' fill='none' xmlns='http://www.w3.org/2000/svg'><g filter='url(#filter0_d_469_10924)'><rect class='rect-next' width='36' height='36' rx='6' transform='matrix(-1 0 0 1 44.4688 8.56641)' fill='white'/></g><path class='path-next' d='M24.4688 32.5664L30.4688 26.5664L24.4688 20.5664' stroke='#9EAAB7' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'/><defs><filter id='filter0_d_469_10924' x='0.46875' y='0.566406' width='52' height='52' filterUnits='userSpaceOnUse' color-interpolation-filters='sRGB'><feFlood flood-opacity='0' result='BackgroundImageFix'/><feColorMatrix in='SourceAlpha' type='matrix' values='0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0' result='hardAlpha'/><feOffset/><feGaussianBlur stdDeviation='4'/><feComposite in2='hardAlpha' operator='out'/><feColorMatrix type='matrix' values='0 0 0 0 0.137255 0 0 0 0 0.164706 0 0 0 0 0.203922 0 0 0 0.2 0'/><feBlend mode='normal' in2='BackgroundImageFix' result='effect1_dropShadow_469_10924'/><feBlend mode='normal' in='SourceGraphic' in2='effect1_dropShadow_469_10924' result='shape'/></filter></defs></svg></button> ",
   });
-
 
   $(".slick-prev").hover(function () {
     $(".rect-prev").toggleClass("actived");
@@ -64,5 +66,4 @@ button.addEventListener("click", () => {
 close.addEventListener("click", () => {
   form.classList.remove("open");
   popup.classList.remove("popup__open");
-  
 });
